04269b2eda18fa32b89de33da75861bfb1be764a
thavao <othavioact@gmail.com>
2024-04-18 16:07:17 -0300

    Add project files.

---
+++ Ex1/Program.cs
@@ -0,0 +1,17 @@
+﻿//1 - faça um algoritmo que leia um numero e informe se é par ou ímpar
+
+int numero;
+
+Console.Write("Por favor, digite um numero inteiro: ");
+
+numero = int.Parse(Console.ReadLine());
+
+Console.Write("O número digitado é ");
+
+if (numero % 2 == 0)
+    Console.WriteLine("par!");
+else
+    Console.WriteLine("ímpar!");
+
+Console.WriteLine("Pressione Enter para encerrar...");
+Console.ReadLine();
